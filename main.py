4a79dc928df40c921f62a45b1e65e2714c844b01
Darshak Patel <102433527+TheShak5@users.noreply.github.com>
2023-05-20 10:30:10 -0400

    commenting

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
+# Import statements
 from pytube import YouTube
 from sys import argv
 
 link = argv[1]
 yt = YouTube(link)
+download_path = '/Users/patel/Downloads'
 
-print("Title: ", yt.title)
+print("Title: ", yt.title) # title of the YouTube Video
 
-print("View: ", yt.views)
+print("View: ", yt.views) # number of views the video has
 
-yd = yt.streams.get_highest_resolution()
+yd = yt.streams.get_highest_resolution() # getting highest resolution of the given video
 
 # ADD FOLDER HERE
-yd.download('/Users/patel/Downloads')
+yd.download(download_path) # downloading the video to the given download path
